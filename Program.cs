f5d295bd883c72e194b16146ffb05a7fc2cb1e69
fireheadmx <fireheadmx@gmail.com>
2015-03-09 15:47:54 -0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,212 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem92 p92 = new Problem92(2, 10000000);
+            //Console.WriteLine(p92.Run().ToString());
+            //Console.ReadLine();
+
+            //Problem40 p40 = new Problem40(1000000);
+            //Console.WriteLine(p40.Run().ToString());
+            //Console.ReadLine();
+
+            //Problem39 p39 = new Problem39();
+            //Console.WriteLine(p39.Run(1000));
+            //Console.ReadLine();
+
+            //Problem34 p34 = new Problem34();
+            //Console.WriteLine(p34.Run());
+            //Console.ReadLine();
+
+            //Problem23 p23 = new Problem23();
+            //Console.WriteLine(p23.Run());
+            //Console.ReadLine();
+
+            //Problem27 p27 = new Problem27();
+            //p27.Run();
+
+            //Problem33 p33 = new Problem33();
+            //p33.Run();
+            //Console.ReadLine();
+
+            //Problem43 p43 = new Problem43();
+            //Console.WriteLine("= " + p43.Run());
+            //Console.ReadLine();
+
+            //Problem42 p42 = new Problem42();
+            //Console.WriteLine("= " + p42.Run());
+            //Console.ReadLine();
+
+            //Problem52 p52 = new Problem52();
+            //Console.WriteLine(p52.Run());
+            //Console.ReadLine();
+
+            //Problem41 p41 = new Problem41();
+            //Console.WriteLine(p41.Run());
+            //Console.ReadLine();
+
+            //Problem53 p53 = new Problem53();
+            //Console.WriteLine(p53.Run());
+            //Console.ReadLine();
+
+            //Problem56 p56 = new Problem56();
+            //Console.WriteLine(p56.Run());
+            //Console.ReadLine();
+
+            //Problems.Problem54 p54 = new Problems.Problem54();
+            //Console.WriteLine(p54.Run());
+            //Console.ReadLine();
+
+            //Problems.Problem55 p55 = new Problems.Problem55();
+            //Console.WriteLine(p55.Run());
+            //Console.ReadLine();
+
+            //Problems.Problem26 p26 = new Problems.Problem26();
+            //Console.WriteLine(p26.Run());
+            //Console.ReadLine();
+
+            //Problems.Problem32 p32 = new Problems.Problem32();
+            //Console.WriteLine(p32.Run());
+            //Console.ReadLine();
+
+            //(new Problems.Problem49()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem50()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem62()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem46()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem47()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem38()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem57()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem97()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem67()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem89()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem63()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem77()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem206()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem71()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem145()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem69()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem73()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem72()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem74()).Run();
+            //Console.ReadLine();
+            //(new Problems.Problem112()).Run();
+            //Console.ReadLine();
+
+            // Unsolved
+            //(new Problems.Problem94()).Run();
+            //Console.ReadLine();
+
+            //(new Problems.Problem59()).Run();
+
+            //(new Problems.Problem81()).Run();
+
+            //Unsolved
+            //(new Problems.Problem243()).Run();
+
+            //(new Problems.Problem58()).Run();
+
+            //(new Problems.Problem82()).Run();
+
+            //(new Problems.Problem70()).Run();
+
+            //(new Problems.Problem61()).Run();
+
+            //(new Problems.Problem104()).Run(); //329468
+
+            // Taking TOO long
+            //(new Problems.Problem113()).Run();
+
+            //(new Problems.Problem99()).Run();
+            //(new Problems.Problem102()).Run();
+            //(new Problems.Problem95()).Run();
+            
+            // Would take days to run
+            //(new Problems.Problem290()).Run();
+
+            //(new Problems.Problem60()).Run();
+            //(new Problems.Problem125()).Run();
+            //(new Problems.Problem120()).Run();
+            //(new Problems.Problem123()).Run();
+            //(new Problems.Problem87()).Run();
+
+            // Overflow
+            //(new Problems.Problem146()).Run();
+
+            //(new Problems.Problem119()).Run();
+            //(new Problems.Problem108()).Run();
+            //(new Problems.Problem110()).Run();
+            //(new Problems.Problem203()).Run();
+            //(new Problems.Problem179()).Run();
+
+            //(new Problems.Problem85()).Run();
+
+            // Unresolved, worked upto f(307)
+            //(new Problems.Problem66()).Run();
+
+            //(new Problems.Problem142()).Run();
+
+            //(new Problems.Problem83()).Run();
+            //(new Problems.Problem51()).Run();
+            //Problems.Problem134.Run();
+            
+            //Unsolved: Sqrt(0.5)*10^12 is not valid
+            //Problems.Problem100.Run();
+
+
+            //Problems.Problem173.Run(); //#100
+            //Problems.Problem131.Run(); //#101
+
+            //Problems.Problem153.Run(); // Would take decades to run
+            //Problems.Problem1.Run();
+
+            //Problems.Problem148.Run();// Run up to 78K
+
+            //Problems.Problem68.Run(); // Picked it up again 2/11/2015
+            //Problems.Problem80.Run();
+            
+        }
+    }
+}
