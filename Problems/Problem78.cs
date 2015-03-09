ba07c746864fe3d86f8a80bf11b8f41452eca140
fireheadmx <fireheadmx@gmail.com>
2015-03-09 16:06:36 -0600

    Create Problem78.cs

---
+++ Problems/Problem78.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler.Problems
+{
+    class Problem78
+    {
+        private int PermCoins(int n)
+        {
+            int count = 1; // One group of n
+            for (int i = n-1; i >= 1; i++)
+            {
+                
+
+            }
+            return count;
+        }
+    }
+}
