29a210633386e091704105e781f3168c9655311e
fireheadmx <fireheadmx@gmail.com>
2015-03-20 15:59:45 -0600

    Update Problem491.cs

--- Problems/Problem491.cs
+++ Problems/Problem491.cs
@@ -9,19 +9,18 @@ namespace ProjectEuler.Problems
     class Problem491
     {
 
-
-
         public static void Run()
         {
+            bool printOutput = true;
             DateTime start = DateTime.Now;
             int[] dblpan = {1, 0, 0, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9};
-
-            //int[] dblpan = { 4,0,5,6,1,8,1,7,7,0,3,8,2,3,5,6,4,9,2,9  };
-
             BigInteger counter = 0;
-
+            BigInteger permCounter = 0;
+            // Max Reached
+            //dblpan = new int[]{1,0,0,1,7,8,4,3,7,4,8,5,9,2,6,2,5,3,9,6};
+            //counter = BigInteger.Parse("5535256117");
             do {
-
+                permCounter++;
                 // Alternating sum of the number, left to right
                 int altSum = dblpan[0] - dblpan[1] +
                     dblpan[2] - dblpan[3] + dblpan[4] - dblpan[5] +
@@ -44,14 +43,21 @@ namespace ProjectEuler.Problems
             }
             while(Permutation.NextPermutation(dblpan));
 
-            using (StreamWriter sw = new StreamWriter("Output/p491.txt", true))
+            DateTime end = DateTime.Now;
+
+            if (printOutput)
             {
-                DateTime end = DateTime.Now;
-                sw.WriteLine("Total: {0}", counter);
-                sw.WriteLine("Time: {0}h, {1}m, {2}s", (start - end).TotalHours, (start - end).Minutes, (start - end).Seconds);
+                using (StreamWriter sw = new StreamWriter("Output/p491.txt", true))
+                {
+                    sw.WriteLine("Permutations: {0}", permCounter);
+                    sw.WriteLine("Total %11=0: {0}", counter);
+                    sw.WriteLine("Time: {0}h, {1}m, {2}s", (start - end).TotalHours, (start - end).Minutes, (start - end).Seconds);
+                }
             }
 
+            Console.WriteLine("Permutations: {0}", permCounter);
             Console.WriteLine("Total: {0}", counter);
+            Console.WriteLine("Time: {0}h, {1}m, {2}s", (start - end).TotalHours, (start - end).Minutes, (start - end).Seconds);
             Console.ReadLine();
 
         }
